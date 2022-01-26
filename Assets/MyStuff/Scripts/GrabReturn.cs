28c3d5cd82ef265c2f5f17f0b50322e68c779da4
Kevin Coursey <kevinscoursey@gmail.com>
2022-01-25 20:54:50 -0800

    Fixed omnidirectional lever

---
+++ Assets/MyStuff/Scripts/GrabReturn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.Serialization;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class GrabReturn : MonoBehaviour
+{
+    public Quaternion defaultQuaternion;
+    public Transform defaultTransform;
+    public GameObject grabbableObject;
+    public float speed = 1f;
+    private void FixedUpdate()
+    {
+        if(!grabbableObject.GetComponent<XRGrabInteractable>().isSelected)
+        {
+            //Quaternion.identity.Set(defaultQuaternion.x, defaultQuaternion.y, defaultQuaternion.z, defaultQuaternion.w);
+            //transform.SetPositionAndRotation(defaultTransform.position, defaultTransform.rotation);
+            transform.position = Vector3.Lerp(transform.position, defaultTransform.position, speed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, defaultTransform.rotation, speed);
+        }
+    }
+}
