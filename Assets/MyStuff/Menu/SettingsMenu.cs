79f3069843dac4ebb2bb87f08e587eb6c6936a70
Kevin Coursey <kevinscoursey@gmail.com>
2022-02-11 16:24:06 -0800

    Audio settings implementation

--- Assets/MyStuff/Menu/SettingsMenu.cs
+++ Assets/MyStuff/Menu/SettingsMenu.cs
@@ -2,44 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
-    public void SetVolume(string _audioType, float volume)
+    public TMP_Text masterVolumeText;
+    public TMP_Text gameplayVolumeText;
+    public TMP_Text musicVolumeText;
+    public TMP_Text multiplayerVolumeText;
+    public Toggle friendlyChatVolumeToggle;
+    public Toggle neutralChatVolumeToggle;
+    public Toggle enemyChatVolumeToggle;
+
+
+    private float ConvertFloatToDecible(float input)
     {
-        switch (_audioType)
-        {
-            case ("master"):
-                audioMixer.SetFloat("MasterVolume", volume);
-                break;
-            case ("gameplay"):
-                audioMixer.SetFloat("GameplayVolume", volume);
-                break;
-            case ("music"):
-                audioMixer.SetFloat("MusicVolume", volume);
-                break;
-            case ("multiplayer"):
-                audioMixer.SetFloat("MultiplayerVolume", volume);
-                break;
-            default:
-                break;
-        }
+        return Mathf.Log10(input) * 20;
+    }
+    private int ConvertDecibleToPercent(float decible)
+    {
+        return Mathf.RoundToInt((decible + 80f) * 1.25f);
     }
     public void SetMasterVolume(float volume)
     {
+        volume = ConvertFloatToDecible(volume);
         audioMixer.SetFloat("MasterVolume", volume);
+        masterVolumeText.SetText($"Master Volume\n{ConvertDecibleToPercent(volume)} %");
     }
     public void SetGameplayVolume(float volume)
     {
+        volume = ConvertFloatToDecible(volume);
         audioMixer.SetFloat("GameplayVolume", volume);
+        gameplayVolumeText.SetText($"Game Volume\n{ConvertDecibleToPercent(volume)} %");
     }
     public void SetMusicVolume(float volume)
     {
+        volume = ConvertFloatToDecible(volume);
         audioMixer.SetFloat("MusicVolume", volume);
+        musicVolumeText.SetText($"Music Volume\n{ConvertDecibleToPercent(volume)} %");
     }
     public void SetMultiplayerVolume(float volume)
     {
+        volume = ConvertFloatToDecible(volume);
         audioMixer.SetFloat("MultiplayerVolume", volume);
+        multiplayerVolumeText.SetText($"Multiplayer Volume\n{ConvertDecibleToPercent(volume)} %");
+    }
+    public void ToggleFriendlyChat()
+    {
+        float volume = friendlyChatVolumeToggle.isOn ? 0f : -80f;
+        audioMixer.SetFloat("FriendlyChatVolume", volume);
+    }
+    public void ToggleNeutralChat()
+    {
+        float volume = neutralChatVolumeToggle.isOn ? 0f : -80f;
+        audioMixer.SetFloat("NeutralChatVolume", volume);
+    }
+    public void ToggleEnemyChat()
+    {
+        float volume = enemyChatVolumeToggle.isOn ? 0f : -80f;
+        audioMixer.SetFloat("EnemyChatVolume", volume);
     }
 }
